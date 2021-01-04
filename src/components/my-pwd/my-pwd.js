3253fb6db04655a6d48be1b585ebff128d8e6f4c
Rebecca Axelsson <ra223ai@student.lnu.se>
2021-01-04 10:54:18 +0100

    Add images

--- src/components/my-pwd/my-pwd.js
+++ src/components/my-pwd/my-pwd.js
@@ -11,13 +11,26 @@
 const template = document.createElement('template')
 template.innerHTML = `
   <style>
-      .pwd-container {
-        height: 100vh;
-        width: 100vw;
-        background-color: teal;
-      }
+    .pwd-container {
+      height: 100vh;
+      width: 100vw;
+      background-image: url('./images/sky-wallpaper.jpg');
+    }
+    .dock {
+        background-color: rgb(75, 74, 74);
+        border: 1px solid grey;
+        border-radius: 10px 10px 0px 0px;
+        width: 60vw;
+        height: 10vh;
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        transform: translate(-50%, 0);
+    }
   </style>
   <div class="pwd-container">
+      <div class="dock">
+      </div>
   </div>
   `
 /**
